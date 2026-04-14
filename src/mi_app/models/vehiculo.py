f74783cc81678493cea82142391119cd05a138bc
BryanGC <bryangarciacastano@gmail.com>
2026-04-14 10:49:44 -0500

    Cambio en la clase vehiculo.py, ya es una tabla creada por SQLMODEL

--- src/mi_app/models/vehiculo.py
+++ src/mi_app/models/vehiculo.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from sqlmodel import SQLModel, Field, Session, create_engine, select
 from dataclasses import dataclass
 from enum import Enum
 from src.mi_app.exceptions import PrecioInvalidoError
@@ -8,31 +11,12 @@ class EstadoVehiculo(Enum):
     RESERVADO = "Reservado"
     VENDIDO = "Vendido"
 
-@dataclass
-class VehiculoUsado:
-    """
-    Entidad principal que representa un carro dentro del concesionario.
-    Se autovalida al momento de ser instanciada.
-    """
-    id_vehiculo: int
-    marca: str
+class Vehiculo(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    marca: str = Field(index=True)
     modelo: str
     anio: int
-    precio: float
     kilometraje: int
-    estado: EstadoVehiculo = EstadoVehiculo.DISPONIBLE
-
-    def __post_init__(self) -> None:
-        """Ejecuta validaciones automáticas al instanciar el vehículo."""
-        self._validar_precio()
-        self._validar_kilometraje()
-
-    def _validar_precio(self) -> None:
-        """Verifica que el precio no sea un número negativo."""
-        if self.precio < 0:
-            raise PrecioInvalidoError("El precio no puede ser negativo.")
-
-    def _validar_kilometraje(self) -> None:
-        """Verifica que el kilometraje no sea un número negativo."""
-        if self.kilometraje < 0:
-            raise ValueError("El kilometraje no puede ser negativo.")
+    precio: int
+    color: Optional[str] = None
+    estado: EstadoVehiculo = Field(default=EstadoVehiculo.DISPONIBLE)
