7f492a0a2e0af9eca4bd7cd28248f856c3185ec0
BryanGC <bryangarciacastano@gmail.com>
2026-04-14 11:05:13 -0500

    Metodo de validacion por pydantic en el archivo vehiculo.py

--- src/mi_app/models/vehiculo.py
+++ src/mi_app/models/vehiculo.py
@@ -1,15 +1,17 @@
 from typing import Optional
 
 from sqlmodel import SQLModel, Field, Session, create_engine, select
-from dataclasses import dataclass
+from pydantic import field_validator
 from enum import Enum
 from src.mi_app.exceptions import PrecioInvalidoError
+
 
 class EstadoVehiculo(Enum):
     """Representa los posibles estados de un vehículo en el inventario."""
     DISPONIBLE = "Disponible"
     RESERVADO = "Reservado"
     VENDIDO = "Vendido"
+
 
 class Vehiculo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -20,3 +22,17 @@ class Vehiculo(SQLModel, table=True):
     precio: int
     color: Optional[str] = None
     estado: EstadoVehiculo = Field(default=EstadoVehiculo.DISPONIBLE)
+
+    @field_validator("precio")
+    @classmethod
+    def validar_precio(cls, value: int) -> int:
+        if value < 0:
+            raise PrecioInvalidoError("El precio no puede ser negativo.")
+        return value
+
+    @field_validator("kilometraje")
+    @classmethod
+    def validar_kilometraje(cls, value: int) -> int:
+        if value < 0:
+            raise ValueError("El kilometraje no puede ser negativo.")
+        return value
