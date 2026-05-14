d0feb9f0a86f13bf2ef43e25a846bffa3599ee51
bryangc <bryangarciacastano@gmail.com>
2026-05-14 16:12:20 -0500

    Main corregido para poder correr el codigo

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
-from api.routers import vehiculos # Luego importamos clientes y ventas
+from api.routers import vehiculos_router, clientes_router, ventas_router
+from api.storage.database import engine
+from sqlmodel import SQLModel
 
-app = FastAPI(title="API Concesionario Medellín")
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
-# Conectar las rutas
-app.include_router(vehiculos.router)
+app = FastAPI(
+    title="🚗 API AG Vehiculos Usados",
+    description="Sistema modular para la gestión de vehículos, clientes y ventas.",
+    version="1.0.0"
+)
 
-@app.get("/")
-def home():
-    return {"mensaje": "API funcionando. Ve a /docs para la documentación."}
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+app.include_router(vehiculos_router.router)
+app.include_router(clientes_router.router)
+app.include_router(ventas_router.router)
+
+@app.get("/", tags=["Inicio"])
+def read_root():
+    return {"mensaje": "¡Bienvenido al sistema del Concesionario! Visita /docs para interactuar."}
